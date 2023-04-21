4cf4122c9bb50c44ad37c361498e7493b38ab548
onepunchcat <115331116+onepunchcat@users.noreply.github.com>
2023-04-21 08:19:31 +0000

    fix: maybe it is mutation

--- src/server/routers/hosting-worker.router.ts
+++ src/server/routers/hosting-worker.router.ts
@@ -3,5 +3,5 @@ import { getByIdSchema } from '../schema/base.schema';
 import { createModelAppHandler } from '../controllers/hosting-worker.controller';
 
 export const hostingWorkerRouter = router({
-  createModelApp: publicProcedure.input(getByIdSchema).query(createModelAppHandler),
+  createModelApp: publicProcedure.input(getByIdSchema).mutation(createModelAppHandler),
 });
