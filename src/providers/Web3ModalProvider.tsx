1b744c9f3d1fe7942c2a637da82cc1e6c3a2be8e
xiaotian <istianlei@qq.com>
2023-05-11 14:39:39 +0800

    perf: upgrade dependencies

--- src/providers/Web3ModalProvider.tsx
+++ src/providers/Web3ModalProvider.tsx
@@ -1,7 +1,7 @@
 import { EthereumClient, w3mConnectors, w3mProvider } from '@web3modal/ethereum';
 import { Web3Modal } from '@web3modal/react';
 import { Fragment } from 'react';
-import { configureChains, createClient, WagmiConfig } from 'wagmi';
+import { configureChains, createConfig, WagmiConfig } from 'wagmi';
 import { mainnet, goerli } from 'wagmi/chains';
 import { env } from '~/env/client.mjs';
 import { infuraProvider } from 'wagmi/providers/infura';
@@ -9,21 +9,21 @@ import { infuraProvider } from 'wagmi/providers/infura';
 export const chains = [mainnet, goerli];
 const projectId = env.NEXT_PUBLIC_WALLET_CONNECT_ID;
 
-const { provider } = configureChains(chains, [
+const { publicClient } = configureChains(chains, [
   infuraProvider({ apiKey: env.NEXT_PUBLIC_INFURA_API_KEY }),
   w3mProvider({ projectId }),
 ]);
-const wagmiClient = createClient({
+const wagmiConfig = createConfig({
   autoConnect: true,
   connectors: w3mConnectors({ projectId, version: 1, chains }),
-  provider,
+  publicClient,
 });
-const ethereumClient = new EthereumClient(wagmiClient, chains);
+const ethereumClient = new EthereumClient(wagmiConfig, chains);
 
 export function Web3ModalProvider({ children }: { children: React.ReactNode }) {
   return (
     <Fragment>
-      <WagmiConfig client={wagmiClient}>{children}</WagmiConfig>
+      <WagmiConfig config={wagmiConfig}>{children}</WagmiConfig>
       <Web3Modal projectId={projectId} ethereumClient={ethereumClient} />
     </Fragment>
   );
