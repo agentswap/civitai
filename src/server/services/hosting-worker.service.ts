c7db5a09d26aba3dc68b4349d1f5a025ccf7274a
onepunchcat <115331116+onepunchcat@users.noreply.github.com>
2023-05-23 06:50:20 +0000

    feat: add update model app method

--- src/server/services/hosting-worker.service.ts
+++ src/server/services/hosting-worker.service.ts
@@ -2,71 +2,116 @@ import { env } from '~/env/server.mjs';
 import { dbRead, dbWrite } from '../db/client';
 import { throwNotFoundError } from '../utils/errorHandling';
 import { createLogger } from '~/utils/logging';
-import { ModelAppStates } from '@prisma/client';
+import { ModelApp, ModelAppStates } from '@prisma/client';
 
 const log = createLogger('hosting-worker', 'blue');
-const hostingWorkerUrl = new URL(env.HOSTING_WORKER_URL).origin;
+const modelAppUrl = new URL('/model-app', env.HOSTING_WORKER_URL).toString();
 
+async function findModelAppById(id: number) {
+  const modelApp = await dbRead.modelApp.findUnique({ where: { id } });
+  if (!modelApp) {
+    throw throwNotFoundError(`Could not find model app with id ${id}`);
+  }
+  return modelApp;
+}
+
+async function setModelAppState(id: number, state: ModelAppStates) {
+  await dbWrite.modelApp.update({ where: { id }, data: { state } });
+}
+
+async function updateModelAppImageName(id: number, oldName: string | null, newName: string) {
+  if (!oldName || oldName !== newName) {
+    log(`Updating model app ${id} image name ${newName}`);
+    await dbWrite.modelApp.update({
+      where: { id },
+      data: { imageName: newName },
+    });
+  }
+}
+
+type ModelAppApiBody = Pick<ModelApp, 'id' | 'name' | 'url'>;
 export type CreateModelAppResponse = {
   id: number;
   port: number;
   imageName: string;
 };
-
 type CreateModelAppError = {
   statusCode: number;
   error: string;
   message: string;
 };
-
-export const hostModelApp = async (appId: number) => {
-  const modelAppInfo = await dbRead.modelApp.findUnique({ where: { id: appId } });
-
-  if (!modelAppInfo) {
-    throw throwNotFoundError(`Could not find model app with id ${appId}`);
+async function createModelApp(body: ModelAppApiBody): Promise<CreateModelAppResponse> {
+  const { id, name, url } = body;
+  const response = await fetch(modelAppUrl, {
+    method: 'POST',
+    body: JSON.stringify({ id, name, url }),
+    headers: { 'Content-Type': 'application/json' },
+  });
+  if (!response.ok) {
+    const message = `Failed to create model app ${id} with name ${name} and url ${url}`;
+    log(message);
+    const error: CreateModelAppError = await response.json();
+    log(`Hosting worker responded with error`, error);
+    throw new Error(message, { cause: error });
   }
-
+  return await response.json();
+}
+export async function hostModelApp(appId: number) {
+  const modelAppInfo = await findModelAppById(appId);
   const { id, name, url, imageName } = modelAppInfo;
 
-  try {
-    // Set model app state to building
-    await dbWrite.modelApp.update({
-      where: { id },
-      data: { state: ModelAppStates.Building },
-    });
+  // Set model app state to building
+  await setModelAppState(id, ModelAppStates.Building);
 
-    log(`Creating model app ${id} with name ${name} and url ${url}`);
-    // Send request to hosting worker
-    const response = await fetch(`${hostingWorkerUrl}/model-app`, {
-      method: 'POST',
-      body: JSON.stringify({ id, name, url }),
-      headers: { 'Content-Type': 'application/json' },
-    });
+  log(`Creating model app ${id} with name ${name} and url ${url}`);
+  // Send request to hosting worker
+  const data = await createModelApp({ id, name, url });
+  log(`Created model app ${id} with name ${name} and url ${url} on port ${data.port}`);
 
-    if (!response.ok) {
-      const error: CreateModelAppError = await response.json();
-      log(`Failed to create model app ${id} with name ${name} and url ${url}`);
-      log(
-        `Hosting worker responded with status code "${error.statusCode} (${error.error})" and message "${error.message}"`
-      );
-      throw new Error(
-        `Failed to create model app ${id} cause: ${error.statusCode} (${error.error}) ${error.message}`
-      );
-    }
+  await updateModelAppImageName(id, imageName, data.imageName);
 
-    const data: CreateModelAppResponse = await response.json();
-    log(`Created model app ${id} with name ${name} and url ${url} on port ${data.port}`);
+  return data;
+}
 
-    if (!imageName || imageName !== data.imageName) {
-      log(`Updating model app ${id} image name ${data.imageName}`);
-      await dbWrite.modelApp.update({
-        where: { id },
-        data: { imageName: data.imageName },
-      });
-    }
-
-    return data;
-  } catch (error) {
-    throw error;
-  }
+export type UpdateModelAppResponse = {
+  id: number;
+  port: number;
+  imageName: string;
 };
+type UpdateModelAppError = {
+  statusCode: number;
+  error: string;
+  message: string;
+};
+async function updateModelApp(body: ModelAppApiBody): Promise<UpdateModelAppResponse> {
+  const { id, name, url } = body;
+  const response = await fetch(modelAppUrl, {
+    method: 'PUT',
+    body: JSON.stringify({ id, name, url }),
+    headers: { 'Content-Type': 'application/json' },
+  });
+  if (!response.ok) {
+    const message = `Failed to update model app ${id} with name ${name} and url ${url}`;
+    log(message);
+    const error: UpdateModelAppError = await response.json();
+    log(`Hosting worker responded with error`, error);
+    throw new Error(message, { cause: error });
+  }
+  return await response.json();
+}
+export async function updateModelAppInfo(appId: number) {
+  const modelAppInfo = await findModelAppById(appId);
+  const { id, name, url, imageName } = modelAppInfo;
+
+  // Set model app state to building
+  await setModelAppState(id, ModelAppStates.Building);
+
+  log(`Updating model app ${id} with name ${name} and url ${url}`);
+  // Send request to hosting worker
+  const data = await updateModelApp({ id, name, url });
+  log(`Updated model app ${id} with name ${name} and url ${url} on port ${data.port}`);
+
+  await updateModelAppImageName(id, imageName, data.imageName);
+
+  return data;
+}
